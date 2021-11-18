90c49877f088307e1cf0600e2f348ab558613d60
discodyer <cody23333@gmail.com>
2021-11-18 20:13:22 +0800

    做出了一些更改，懒得解释了

--- btcar/btcar-demo-2/btcar-demo-2.ino
+++ btcar/btcar-demo-2/btcar-demo-2.ino
@@ -1,46 +1,73 @@
+#include <Arduino_FreeRTOS.h>      //using freertos to realize multithreading 
+
 #include "L298NX4.h"
 #include "L298N.h"
-//定义第一组控制引脚（M1
+
+//init ports of M1
 const unsigned int IN_A1 = 4;
 const unsigned int IN_A2 = 2;
 const unsigned int EN_A  = 3;
-//定义第二组控制引脚（M2
+//init ports of M2
 const unsigned int IN_B1 = 11;
 const unsigned int IN_B2 = 10;
 const unsigned int EN_B  = 5;
-//定义第三组控制引脚（M3
+//init ports of M3
 const unsigned int IN_C1 = 8;
 const unsigned int IN_C2 = 7;
 const unsigned int EN_C  = 6;
-//定义第四组控制引脚（M4
+//init ports of M4
 const unsigned int IN_D1 = 13;
 const unsigned int IN_D2 = 12;
 const unsigned int EN_D  = 9;
+
+
+/*
+         ↑Front↑
+      ┌----------┐
+   A ▤│M1     M2│▤ B
+      │          │
+      │          │
+      │          │
+   C ▤│M3     M4│▤ D
+      └----------┘
+*/
+
 
 L298NX4 motors( EN_A, IN_A1, IN_A2,
                 EN_B, IN_B1, IN_B2, 
                 EN_C, IN_C1, IN_C2, 
                 EN_D, IN_D1, IN_D2);
 
-//定义两种速度
+
+
+//init two speed
 unsigned short LOW_SPEED = 55;
 unsigned short HIGH_SPEED = 255;
 
 
+void printSomeInfo();//print motor info to serial
+void testAllMotors();//test all motors 
+
+
+
 void setup() {
+  
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB, on LEONARDO, MICRO, YUN, and other 32u4 based boards.
 
-Serial.begin(9600);
-
-  // Wait for Serial Monitor to be opened
-  while (!Serial)
-  {
-    //do nothing
-  }
   // Set initial speed for ALL motors
+  
+  }
   motors.setSpeed(80);
 }
 
+
 void loop() {
+
+  // Empty. Things are done in Tasks.
 
 }
 
@@ -57,4 +84,50 @@ void printSomeInfo()
   Serial.print(motors.isMovingB() ? "YES" : "NO");
   Serial.print(" at speed = ");
   Serial.println(motors.getSpeedB());
+}
+
+
+/*
+test all motors
+*/
+void testAllMotors()
+{
+  motors.setSpeed(255);
+  motors.forward();
+  printSomeInfo();
+  delay(2000);
+  motors.stop();
+  
+  motors.backward();
+  printSomeInfo();
+  delay(2000);
+  motors.stop();
+
+  motors.forwardA();
+  delay(1000);
+  motors.stop();
+  motors.backwardA();
+  delay(1000);
+  motors.stop();
+
+  motors.forwardB();
+  delay(1000);
+  motors.stop();
+  motors.backwardB();
+  delay(1000);
+  motors.stop();
+
+  motors.forwardC();
+  delay(1000);
+  motors.stop();
+  motors.backwardC();
+  delay(1000);
+  motors.stop();
+
+  motors.forwardD();
+  delay(1000);
+  motors.stop();
+  motors.backwardD();
+  delay(1000);
+  motors.stop();
 }
